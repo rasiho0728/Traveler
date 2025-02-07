3095c5b38725b4b2b4400e7256edf8e8a001cc93
EUIJIN1209 <chldmlwls111@naver.com>
2025-02-07 12:56:04 +0900

    new file:   admin/client/src/assets/images/transport/subway.png 	modified:   admin/client/src/components/Data/Transport.js 	modified:   admin/client/src/screens/Projects/Transport.tsx

--- admin/client/src/components/Data/Transport.js
+++ admin/client/src/components/Data/Transport.js
@@ -1,74 +1,12 @@
 
-import Avatar8 from "../../assets/images/xs/avatar8.jpg";
-import lgAvatar1 from "../../assets/images/lg/avatar1.jpg";
-import lgAvatar3 from "../../assets/images/lg/avatar3.jpg";
-import lgAvatar4 from "../../assets/images/lg/avatar4.jpg";
-import lgAvatar5 from "../../assets/images/lg/avatar5.jpg";
-import lgAvatar7 from "../../assets/images/lg/avatar7.jpg";
-import lgAvatar8 from "../../assets/images/lg/avatar8.jpg";
-
+import transport from "../../assets/images/transport/subway.png"
 
 
 
 export const ProjectCardData = [
+ 
   {
-    teamImage: [lgAvatar1, lgAvatar3, lgAvatar4, lgAvatar4, Avatar8],
-    logo: "icofont-paint",
-    logoBg: "light-info-bg",
-    title: "UI/UX Design",
-    sl: "Social Geek Made"
-  },
-  {
-    teamImage: [lgAvatar1, lgAvatar3, lgAvatar4, lgAvatar4],
-    logo: "icofont-vector-path",
-    logoBg: "bg-lightgreen",
-    title: "Website Design",
-    sl: "Practice to Perfect"
-  },
-  {
-    teamImage: [lgAvatar1, lgAvatar3, lgAvatar4, lgAvatar8, lgAvatar5, lgAvatar5, lgAvatar7],
-    logo: "icofont-ui-touch-phone",
-    logoBg: "bg-lightyellow",
-    title: "App Development",
-    sl: "Java Dalia"
-  },
-  {
-    teamImage: [lgAvatar1, lgAvatar3],
-    logo: "icofont-tick-boxed",
-    logoBg: "light-success-bg",
-    title: "Quality Assurance",
-    sl: "Switch and Swif"
-  },
-  {
-    teamImage: [lgAvatar1, lgAvatar3, lgAvatar1, lgAvatar3, lgAvatar4, lgAvatar8, lgAvatar5, lgAvatar5, lgAvatar7],
-    logo: "icofont-dashboard-web",
-    logoBg: "light-orange-bg",
-    title: "Development",
-    sl: "Gob Geeklords"
-  },
-  {
-    teamImage: [lgAvatar1, lgAvatar3, lgAvatar4, lgAvatar8, lgAvatar5, lgAvatar5, lgAvatar7],
-    logo: "icofont-tasks",
-    logoBg: "bg-lightblue",
-    title: "Software Testing",
-    sl: "Fast Cad"
-  },
-  {
-    teamImage: [lgAvatar1, lgAvatar3, lgAvatar4, lgAvatar8],
-    logo: "icofont-vector-path",
-    logoBg: "bg-lightgreen",
-    title: "Website Design",
-    sl: "Hex Clan"
-  },
-  {
-    teamImage: [lgAvatar1, lgAvatar3],
-    logo: "icofont-tick-boxed",
-    logoBg: "light-success-bg",
-    title: "Quality Assurance",
-    sl: "Switch and Swif"
-  },
-  {
-    teamImage: [lgAvatar1, lgAvatar3, lgAvatar1, lgAvatar3, lgAvatar4, lgAvatar8, lgAvatar5, lgAvatar5, lgAvatar7],
+    teamImage: [transport],
     logo: "icofont-dashboard-web",
     logoBg: "light-orange-bg",
     title: "Development",
