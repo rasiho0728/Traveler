719a5ab2e74289fd10bff2e0a90e3f2e50bbee2b
ictpass <javabook@naver.com>
2025-02-16 16:58:06 +0900

    modified:   user/back/user/src/main/java/kr/co/user/bus/BusController.java

--- user/back/user/src/main/java/kr/co/user/bus/BusController.java
+++ user/back/user/src/main/java/kr/co/user/bus/BusController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+//2025-02-16수정
 @RestController
 @RequestMapping("/api/bus")
 public class BusController {
