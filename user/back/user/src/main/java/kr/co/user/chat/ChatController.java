b036bac82da3a085fa6c4521c9377cb15ff4c577
rasiho0728 <fghj0728@gmail.com>
2025-02-17 17:11:12 +0900

    modified:   user/back/user/build.gradle 	modified:   user/back/user/src/main/java/kr/co/user/UserApplication.java 	new file:   user/back/user/src/main/java/kr/co/user/chat/Chat.java 	new file:   user/back/user/src/main/java/kr/co/user/chat/ChatBotController.java 	new file:   user/back/user/src/main/java/kr/co/user/chat/ChatController.java 	new file:   user/back/user/src/main/java/kr/co/user/config/WebSocketConfig.java 	new file:   user/back/user/src/main/java/kr/co/user/member/Member.java 	new file:   user/back/user/src/main/java/kr/co/user/security/Role.java 	modified:   user/client/src/Comm/Header.tsx

---
+++ user/back/user/src/main/java/kr/co/user/chat/ChatController.java
@@ -0,0 +1,5 @@
+package kr.co.user.chat;
+
+public class ChatController {
+    
+}
---
+++ user/back/user/src/main/java/kr/co/user/member/Member.java
@@ -0,0 +1,58 @@
+package kr.co.user.member;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import kr.co.user.security.Role;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@Entity
+@SequenceGenerator(name = "member_seq_gen", sequenceName = "member_seq", initialValue = 1, allocationSize = 1)
+public class Member {
+    @Id
+    @GeneratedValue(generator = "member_seq_gen", strategy = GenerationType.SEQUENCE)
+    private Long num;
+
+    @Column(name = "USERNAME", columnDefinition = "varchar2(15)", nullable = false, unique = true)
+    private String userName;
+
+    @Column(name = "PWD", columnDefinition = "varchar2(150)", nullable = false)
+    private String pwd;
+
+    @Column(name = "NAME", columnDefinition = "varchar2(50)", nullable = false)
+    private String name;
+
+    @Column(name = "CODE", columnDefinition = "varchar2(10)", nullable = false)
+    private String code;
+
+    @Column(name = "PHONE", columnDefinition = "varchar2(13)", nullable = false)
+    private String phone;
+
+    @Column(name = "EMAIL", columnDefinition = "varchar2(50)", nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "ROLE", columnDefinition = "varchar2(15) CHECK(ROLE IN ('ADMIN','USER','COALITION'))")
+    private Role role;
+
+    @Column(name = "SOCIALUSER", columnDefinition = "number(1)", nullable = false)
+    private Long socialuser;
+
+    @Column(name = "COMPANY", columnDefinition = "number(1)", nullable = false)
+    private Long company;
+
+    @Column(name = "MDATE", columnDefinition = "date default sysdate", nullable = false)
+    private Date mdate; 
+}
