2c81b9e03abad96bbfd0958504a77ec2f5d7cc03
ictpass01 <rimp3719@gmail.com>
2025-02-18 19:30:56 +0900

    Changes to be committed: 	modified:   user/back/user/src/main/java/kr/co/user/mypage/MypageController.java 	modified:   user/back/user/src/main/java/kr/co/user/mypage/MypageRepository.java 	modified:   user/back/user/src/main/java/kr/co/user/mypage/MypageService.java

--- user/back/user/src/main/java/kr/co/user/mypage/MypageRepository.java
+++ user/back/user/src/main/java/kr/co/user/mypage/MypageRepository.java
@@ -8,8 +8,15 @@ import kr.co.user.chat.Chat;
 import kr.co.user.member.MemberVO;
 
 public interface MypageRepository extends JpaRepository<MemberVO, Long>{
+    // 사용자 정보 조회
+    @Query(value = "SELECT * FROM MemberVO WHERE username = :username", nativeQuery = true)
+    MemberVO findUserByUsername(@Param("username") String username);
     
-    // 채팅내역 최신글 5가지 가져오기
+    // 챗봇과의 최근 대화 5개 조회 (type = 1)
     @Query("SELECT c FROM MemberVO m JOIN m.chatlog c WHERE m.username = :username AND c.type = 1 ORDER BY c.cdate DESC")
     List<Chat> chatbotList(@Param("username") String username);
+
+    // 관리자(직원)과의 최근 대화 5개 조회 (type = 0)
+    @Query("SELECT c FROM MemberVO m JOIN m.chatlog c WHERE m.username = :username AND c.type = 0 ORDER BY c.cdate DESC")
+    List<Chat> adminChatList(@Param("username") String username);
 }
