be9ae1d2628c0600bcf4dd99a8518327c5f032d2
ictpass01 <sed6322@naver.com>
2025-02-18 17:29:54 +0900

    Changes to be committed: 	modified:   user/back/user/build.gradle 	modified:   user/back/user/src/main/java/kr/co/user/community/BackPackController.java

--- user/back/user/src/main/java/kr/co/user/community/BackPackController.java
+++ user/back/user/src/main/java/kr/co/user/community/BackPackController.java
@@ -1,13 +1,22 @@
 package kr.co.user.community;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/backpack")
@@ -16,7 +25,42 @@ public class BackPackController {
     @Autowired
     private BackPackService backpapackService;
 
-    // private final Path uploadDir = Paths.get
+    private final Path uploadDir = Paths.get("/files/logs")
+            .toAbsolutePath().normalize();
+
+    public BackPackController() {
+        try {
+            Files.createDirectories(uploadDir);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to create upload directory", e);
+        }
+    }
+
+    @PostMapping
+    public BackPack createGallery(BackPackVO vo) throws IOException{
+        List<String> imageNames = new ArrayList<>();
+        System.out.println("Logs==========>" + uploadDir);
+        if (vo.getImages() != null) {
+            for (MultipartFile multipartFile : vo.getImages()) {
+                if (!multipartFile.isEmpty()) {
+                    String originalFileName = multipartFile.getOriginalFilename();
+                    System.out.println(originalFileName);
+                    Path destinationFile = uploadDir.resolve(originalFileName).normalize();
+                    try {
+                        Files.copy(multipartFile.getInputStream(), destinationFile);
+                    } catch (FileAlreadyExistsException e) {
+                       System.out.println("파일이 이미 존재 합니다.");
+                    } finally{
+                        imageNames.add(originalFileName);
+                    }
+                }
+            }
+        } else {
+            imageNames.add("");
+        }
+        vo.setImgNames(imageNames);
+        return backpapackService.createBackPack(vo);
+    }
 
     @GetMapping("/list")
     public List<BackPack> getAllBackPackList() {
