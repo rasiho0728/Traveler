05028b076fd44258bcf158737e4de61a6f75a267
ictpass <javabook@naver.com>
2025-02-18 15:00:45 +0900

    modified:   user/back/user/src/main/java/kr/co/user/diary/Diary.java 	modified:   user/back/user/src/main/java/kr/co/user/diary/DiaryPageRepository.java 	modified:   user/back/user/src/main/java/kr/co/user/diary/DiaryRepository.java

--- user/back/user/src/main/java/kr/co/user/diary/DiaryPageRepository.java
+++ user/back/user/src/main/java/kr/co/user/diary/DiaryPageRepository.java
@@ -3,7 +3,8 @@ package kr.co.user.diary;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface DiaryPageRepository extends JpaRepository<DiaryPage, Long>{
     List<DiaryPage> findByDiarynum(Long diarynum);  // 특정 다이어리의 모든 페이지 조회
     
--- user/back/user/src/main/java/kr/co/user/diary/DiaryRepository.java
+++ user/back/user/src/main/java/kr/co/user/diary/DiaryRepository.java
@@ -1,9 +1,11 @@
 package kr.co.user.diary;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
-
+@Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
     List<Diary> findByMambernum(Integer mambernum);//특정 회원의 다이어리 목록 조회
 
