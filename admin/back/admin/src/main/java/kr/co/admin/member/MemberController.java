1f4010577c0948da6e61ae1f06482af4d0a06097
ictpass01 <rimp3719@gmail.com>
2025-02-18 16:48:24 +0900

    Changes to be committed: 	new file:   admin/back/admin/src/main/java/kr/co/admin/member/MemberController.java 	new file:   admin/back/admin/src/main/java/kr/co/admin/member/MemberRepository.java 	new file:   admin/back/admin/src/main/java/kr/co/admin/member/MemberService.java

---
+++ admin/back/admin/src/main/java/kr/co/admin/member/MemberController.java
@@ -0,0 +1,36 @@
+package kr.co.admin.member;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/member")
+public class MemberController {
+    @Autowired
+    private MemberService memberService;
+
+    // @GetMapping //회원목록 불러오기
+    // public List<Member> listMember() {
+    //     return memberService.list();
+    // }
+    @GetMapping // 회원목록 전체데이터 + 페이징처리
+    public ResponseEntity<?> listMember(
+        @RequestParam(defaultValue = "1") int page,
+        @RequestParam(defaultValue = "10") int size
+    ) {
+        return ResponseEntity.ok().body(memberService.getMemberListWithPagination(page, size));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteMember(@RequestParam Long num) {
+        memberService.delete(num);
+        return ResponseEntity.ok().body("회원삭제 완료");
+    }
+}
