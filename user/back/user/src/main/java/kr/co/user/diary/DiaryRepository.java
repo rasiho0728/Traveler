c32002076a96a803ee281ac33b80155e5ebf9239
ictpass <javabook@naver.com>
2025-02-18 15:31:37 +0900

    modified:   user/back/user/src/main/java/kr/co/user/diary/DiaryPageRepository.java 	modified:   user/back/user/src/main/java/kr/co/user/diary/DiaryRepository.java

--- user/back/user/src/main/java/kr/co/user/diary/DiaryRepository.java
+++ user/back/user/src/main/java/kr/co/user/diary/DiaryRepository.java
@@ -1,13 +1,10 @@
 package kr.co.user.diary;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
-@Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
-    List<Diary> findByMambernum(Integer mambernum);//특정 회원의 다이어리 목록 조회
+    // List<Diary> findByMambernum(Integer mambernum);//특정 회원의 다이어리 목록 조회
 
     
 }
