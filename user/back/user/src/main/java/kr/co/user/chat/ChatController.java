2a73211906cb335d6d26e4d8cec15d53add51f0d
rasiho0728 <fghj0728@gmail.com>
2025-02-18 16:26:02 +0900

    modified:   user/back/user/src/main/java/kr/co/user/chat/ChatController.java

--- user/back/user/src/main/java/kr/co/user/chat/ChatController.java
+++ user/back/user/src/main/java/kr/co/user/chat/ChatController.java
@@ -10,6 +10,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/chat")
