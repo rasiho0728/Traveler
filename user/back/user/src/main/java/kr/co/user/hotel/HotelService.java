475baf9f21a41fb982f37eb4953381b33f346c9b
DoYeon05 <ehfussla123@gmail.com>
2025-02-20 18:54:56 +0900

    Changes to be committed: 	modified:   user/back/user/src/main/java/kr/co/user/diary/DiaryPage.java 	modified:   user/back/user/src/main/java/kr/co/user/hotel/HotelController.java 	modified:   user/back/user/src/main/java/kr/co/user/hotel/HotelImage.java 	modified:   user/back/user/src/main/java/kr/co/user/hotel/HotelRepository.java 	modified:   user/back/user/src/main/java/kr/co/user/hotel/HotelService.java 	new file:   user/client/16.3.0 	new file:   user/client/public/images/hotels/hotelbackground1.webp 	new file:   user/client/public/images/hotels/hotelbackground2.jpg 	modified:   user/client/src/Pages/Hotel/Hotel2.tsx 	modified:   user/client/src/Pages/Hotel/HotelDetail2.tsx 	modified:   user/client/src/Transport/MapRoad.tsx 	modified:   user/client/src/css/hotel.css

--- user/back/user/src/main/java/kr/co/user/hotel/HotelService.java
+++ user/back/user/src/main/java/kr/co/user/hotel/HotelService.java
@@ -1,9 +1,11 @@
 package kr.co.user.hotel;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,14 +17,27 @@ public class HotelService {
     @Autowired
     private HotelRepository hotelRepository;
 
-    // 호텔 목록 조회
+    // 호텔 목록 조회 (리스트)
     public List<Hotel> getHotelList() {
         return hotelRepository.findAll();
     }
 
-    // 특정 호텔 정보 조회
-    public Hotel getHotelById(Long id) {
-        return hotelRepository.findById(id).orElse(null);
+    public Hotel getHotelByNum(Long num) {
+        Hotel hotel = hotelRepository.findById(num)
+                .orElseThrow(() -> new RuntimeException("상세보기 실패했습니다."));
+        hotel.setHit(hotel.getHit() + 1);
+        hotelRepository.save(hotel);
+        return hotel;
     }
 
+    public Page<Hotel> getHotelsWithPagination(String searchQuery, int page, int size) {
+        int startRow = (page - 1) * size + 1;
+        int endRow = startRow + size - 1;
+
+        List<Hotel> entity = hotelRepository.findByNameOrLocationContainingOrderByNumDesc(searchQuery, startRow,
+                endRow);
+
+        int totalElements = hotelRepository.countByNameOrLocationContaining(searchQuery);
+        return new PageImpl<>(entity, PageRequest.of(page - 1, size), totalElements);
+    }
 }
